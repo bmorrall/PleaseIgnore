805ce7c3d54af32daeaf6bfc5d32082c3a4e325e
Ben Morrall <bemo56@hotmail.com>
2014-01-16 23:27:24 +1100

    Added features from rails3-devise-rspec-cucumber

--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,13 @@ ActiveRecord::Schema.define(version: 20140116121309) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
