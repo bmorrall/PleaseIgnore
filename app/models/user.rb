3b951693961a2b1fbf60ee1a7444136a56f7bf43
Ben Morrall <bemo56@hotmail.com>
2014-02-09 15:00:23 +1100

    Added user sign in method stubs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,37 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
- validates :name, presence: true
+  validates :name, presence: true
+
+  def facebook_login?
+    false # TODO: Implement OAuth
+  end
+
+  def github_login?
+    false # TODO: Implement OAuth
+  end
+
+  def google_login?
+    false # TODO: Implement OAuth
+  end
+
+  def twitter_login?
+    false # TODO: Implement OAuth
+  end
+
+  def email_required?
+    !(facebook_login? || github_login? || google_login? || twitter_login?)
+  end
+
+  def password_required?
+    email_required? && super
+  end
+
+  def profile_picture(size=128)
+    if email.present?
+      gravatar_id = Digest::MD5.hexdigest(email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    end
+  end
 
 end
