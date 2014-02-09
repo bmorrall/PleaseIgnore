62fb63cec350b48338b8f2480789ea2bafdd4dbd
Ben Morrall <bemo56@hotmail.com>
2014-02-09 15:00:06 +1100

    Added basic hackathon theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require lib/jquery
+//= require lib/bootstrap
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap
-//= require_tree .
+//= require main
