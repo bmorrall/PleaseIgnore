3bb5980b1815ce3cce1d09d10763b5d096e0467e
Ben Morrall <ben.morrall@wavedigital.com.au>
2014-02-28 18:25:45 +1100

    Added full test coverage to users and accounts

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,20 +1,307 @@
 require 'spec_helper'
 
 describe Account do
+  include OmniauthHelpers
 
-  it { should belong_to(:user) }
-
-  it { should validate_presence_of(:uid) }
-  it { should validate_presence_of(:provider) }
-  it 'should validate uniqueness of uid scoped to provider' do
-    FactoryGirl.create(:account)
-    should validate_uniqueness_of(:uid).scoped_to(:provider)
+  describe 'associations' do
+    it { should belong_to(:user) }
   end
-  it { should validate_presence_of(:user_id) }
 
-  it { should allow_value('developer').for(:provider) }
-  it { should allow_value('facebook').for(:provider) }
-  it { should allow_value('github').for(:provider) }
-  it { should allow_value('google_oauth2').for(:provider) }
-  it { should allow_value('twitter').for(:provider) }
+  describe 'validations' do
+    it { should validate_presence_of(:provider) }
+    it { should validate_presence_of(:uid) }
+    it 'should validate uniqueness of uid scoped to provider' do
+      FactoryGirl.create(:account)
+      should validate_uniqueness_of(:uid).scoped_to(:provider)
+    end
+    it { should validate_presence_of(:user_id) }
+
+    it { should allow_value('developer').for(:provider) }
+    it { should allow_value('facebook').for(:provider) }
+    it { should allow_value('github').for(:provider) }
+    it { should allow_value('google_oauth2').for(:provider) }
+    it { should allow_value('twitter').for(:provider) }
+  end
+
+  describe '.find_for_oauth' do
+    context 'with a Facebook auth hash' do
+      let(:auth_hash) { facebook_auth_hash }
+      context 'with an existing account' do
+        let!(:existing_account) { FactoryGirl.create(:facebook_account, uid: auth_hash.uid) }
+        it 'finds the existing_account matching the uid' do
+          Account.find_for_oauth(auth_hash).should eq(existing_account)
+        end
+        it 'updates the existing account with the auth hash' do
+          existing_account.should_receive(:update_from_auth_hash).with(auth_hash)
+          existing_account.should_receive(:save)
+          Account.stub(:find_by_provider_and_uid).and_return(existing_account)
+          Account.find_for_oauth(auth_hash).should eq(existing_account)
+        end
+      end
+      it 'returns nil with no existing Account' do
+        Account.find_for_oauth(auth_hash).should be_nil
+      end
+    end
+    context 'with a Twitter auth hash' do
+      let(:auth_hash) { twitter_auth_hash }
+      context 'with an existing account' do
+        let!(:existing_account) { FactoryGirl.create(:twitter_account, uid: auth_hash.uid) }
+        it 'finds the existing_account matching the uid' do
+          Account.find_for_oauth(auth_hash).should eq(existing_account)
+        end
+        it 'updates the existing account with the auth hash' do
+          existing_account.should_receive(:update_from_auth_hash).with(auth_hash)
+          existing_account.should_receive(:save)
+          Account.stub(:find_by_provider_and_uid).and_return(existing_account)
+          Account.find_for_oauth(auth_hash).should eq(existing_account)
+        end
+      end
+      it 'returns nil with no existing Account' do
+        Account.find_for_oauth(auth_hash).should be_nil
+      end
+    end
+    context 'with a GitHub auth hash' do
+      let(:auth_hash) { github_auth_hash }
+      context 'with an existing account' do
+        let!(:existing_account) { FactoryGirl.create(:github_account, uid: auth_hash.uid) }
+        it 'finds the existing_account matching the uid' do
+          Account.find_for_oauth(auth_hash).should eq(existing_account)
+        end
+        it 'updates the existing account with the auth hash' do
+          existing_account.should_receive(:update_from_auth_hash).with(auth_hash)
+          existing_account.should_receive(:save)
+          Account.stub(:find_by_provider_and_uid).and_return(existing_account)
+          Account.find_for_oauth(auth_hash).should eq(existing_account)
+        end
+      end
+      it 'returns nil with no existing Account' do
+        Account.find_for_oauth(auth_hash).should be_nil
+      end
+    end
+    context 'with a Google auth hash' do
+      let(:auth_hash) { google_auth_hash }
+      context 'with an existing account' do
+        let!(:existing_account) { FactoryGirl.create(:google_account, uid: auth_hash.uid) }
+        it 'finds the existing_account matching the uid' do
+          Account.find_for_oauth(auth_hash).should eq(existing_account)
+        end
+        it 'updates the existing account with the auth hash' do
+          existing_account.should_receive(:update_from_auth_hash).with(auth_hash)
+          existing_account.should_receive(:save)
+          Account.stub(:find_by_provider_and_uid).and_return(existing_account)
+          Account.find_for_oauth(auth_hash).should eq(existing_account)
+        end
+      end
+      it 'returns nil with no existing Account' do
+        Account.find_for_oauth(auth_hash).should be_nil
+      end
+    end
+    it 'raises an exception if the provider does not match the expected value' do
+      expect {
+        Account.find_for_oauth(twitter_auth_hash, 'not_matching_provider')
+      }.to raise_error(Exception, "Provider (twitter) doesn't match expected value: not_matching_provider")
+    end
+  end
+
+  describe '.omniauth_providers' do
+    it { Account.omniauth_providers.should include(:facebook) }
+    it { Account.omniauth_providers.should include(:twitter) }
+    it { Account.omniauth_providers.should include(:github) }
+    it { Account.omniauth_providers.should include(:google_oauth2) }
+    it { Account.omniauth_providers.should_not include(:developer) }
+  end
+
+  describe '.provider_name' do
+    it 'should return Developer for developer' do
+      Account.provider_name('developer').should eq('Developer')
+    end
+    it 'should return Facebook for facebook' do
+      Account.provider_name('facebook').should eq('Facebook')
+    end
+    it 'should return Twitter for twitter' do
+      Account.provider_name('twitter').should eq('Twitter')
+    end
+    it 'should return GitHub for github' do
+      Account.provider_name('github').should eq('GitHub')
+    end
+    it 'should return Google for google_auth2' do
+      Account.provider_name('google_oauth2').should eq('Google')
+    end
+  end
+
+  describe '#account_uid' do
+    describe 'with a Facebook account' do
+      subject { FactoryGirl.build_stubbed(:facebook_account) }
+      it 'returns the name for Facebook' do
+        subject.name = 'Test User'
+        subject.account_uid.should eq('Test User')
+      end
+    end
+    describe 'with a Twitter account' do
+      subject { FactoryGirl.build_stubbed(:twitter_account) }
+      it 'returns the nickname for Twitter' do
+        subject.nickname = '@testuser'
+        subject.account_uid.should eq('@testuser')
+      end
+      it 'prepends an @ to the nickname' do
+        subject.nickname = 'testuser'
+        subject.account_uid.should eq('@testuser')
+      end
+    end
+    describe 'with a GitHub account' do
+      subject { FactoryGirl.build_stubbed(:github_account) }
+      it 'returns the name for GitHub' do
+        subject.nickname = 'testuser'
+        subject.account_uid.should eq('testuser')
+      end
+    end
+    describe 'with a Google account' do
+      subject { FactoryGirl.build_stubbed(:google_account) }
+      it 'returns the name for Google' do
+        subject.name = 'Test User'
+        subject.account_uid.should eq('Test User')
+      end
+    end
+  end
+
+  describe '#enabled?' do
+    it 'should be true for a valid account' do
+      FactoryGirl.build_stubbed(:account).should be_enabled
+    end
+    it 'returns false for an account without a user' do
+      FactoryGirl.build_stubbed(:account, user: nil).should_not be_enabled
+    end
+  end
+
+  describe '#profile_picture' do
+    it 'returns the image' do
+      fake_image = Object.new
+      subject.should_receive(:image).and_return(fake_image)
+      subject.profile_picture.should be(fake_image)
+    end
+  end
+
+  describe '#provider_name' do
+    it 'returns Account.provider_name with the provider' do
+      fake_provider = 'provider'
+      fake_provider_name = 'ProVid3r'
+      subject.provider = fake_provider
+      Account.should_receive(:provider_name).with(fake_provider).and_return(fake_provider_name)
+      subject.provider_name.should be(fake_provider_name)
+    end
+  end
+
+  describe '#update_from_auth_hash' do
+    context 'with a generic auth hash containing info params' do
+      let(:info_params) { {} }
+      let(:auth_hash) { OmniAuth::AuthHash.new({info: info_params}) }
+      it 'should update the name from the auth hash' do
+        info_params[:name] = 'Test User'
+        subject.update_from_auth_hash(auth_hash)
+        subject.name.should eq('Test User')
+      end
+      it 'should update the nickname from the auth hash' do
+        info_params[:nickname] = 'nickname'
+        subject.update_from_auth_hash(auth_hash)
+        subject.nickname.should eq('nickname')
+      end
+      it 'should update the image from the auth hash' do
+        info_params[:image] = 'http://graph.facebook.com/1234567/picture?type=square'
+        subject.update_from_auth_hash(auth_hash)
+        subject.image.should eq('http://graph.facebook.com/1234567/picture?type=square')
+      end
+    end
+    context 'with a Facebook account' do
+      before(:each) { subject.provider = 'facebook' }
+      let(:info_params) { {} }
+      let(:credentials_params) { {} }
+      let(:auth_hash) { OmniAuth::AuthHash.new({provider: 'facebook', info: info_params, credentials: credentials_params}) }
+
+      it 'should set the website from the Facebook url' do
+        website = 'http://www.facebook.com/jbloggs'
+        info_params[:urls] = { Facebook: website }
+        subject.update_from_auth_hash(auth_hash)
+        subject.website.should eq(website)
+      end
+      it 'should set the oauth_token and oauth_expires values' do
+        expires = 3.months.since
+        credentials_params[:token] = 'example_token'
+        credentials_params[:expires_at] = expires.to_i
+        subject.update_from_auth_hash(auth_hash)
+
+        subject.oauth_token.should eq('example_token')
+        subject.oauth_expires_at.to_s.should eq(expires.to_s)
+      end
+    end
+    context 'with a Twitter auth hash' do
+      before(:each) { subject.provider = 'twitter' }
+      let(:info_params) { {} }
+      let(:credentials_params) { {} }
+      let(:auth_hash) { OmniAuth::AuthHash.new({provider: 'twitter', info: info_params, credentials: credentials_params}) }
+
+      it 'should set the website from the Facebook url' do
+        website = 'https://twitter.com/johnqpublic'
+        info_params[:urls] = { Twitter: website }
+        subject.update_from_auth_hash(auth_hash)
+        subject.website.should eq(website)
+      end
+      it 'should set the oauth_token and oauth_secret values' do
+        expires = 3.months.since
+        credentials_params[:token] = 'example_token'
+        credentials_params[:secret] = 'example_secret'
+        subject.update_from_auth_hash(auth_hash)
+
+        subject.oauth_token.should eq('example_token')
+        subject.oauth_secret.should eq('example_secret')
+      end
+    end
+    context 'with a GitHub auth hash' do
+      before(:each) { subject.provider = 'github' }
+      let(:info_params) { {} }
+      let(:credentials_params) { {} }
+      let(:auth_hash) { OmniAuth::AuthHash.new({provider: 'github', info: info_params, credentials: credentials_params}) }
+
+      it 'should set the website from the Facebook url' do
+        website = 'https://github.com/johnqpublic'
+        info_params[:urls] = { Github: website }
+        subject.update_from_auth_hash(auth_hash)
+        subject.website.should eq(website)
+      end
+      it 'should set the oauth_token and oauth_expires values' do
+        expires = 3.months.since
+        credentials_params[:token] = 'example_token'
+        credentials_params[:expires_at] = expires.to_i
+        subject.update_from_auth_hash(auth_hash)
+
+        subject.oauth_token.should eq('example_token')
+        subject.oauth_expires_at.to_s.should eq(expires.to_s)
+      end
+      it 'should set the oauth_token and oauth_secret values' do
+        expires = 3.months.since
+        credentials_params[:token] = 'example_token'
+        credentials_params[:secret] = 'example_secret'
+        subject.update_from_auth_hash(auth_hash)
+
+        subject.oauth_token.should eq('example_token')
+        subject.oauth_secret.should eq('example_secret')
+      end
+    end
+    context 'with a Google auth hash' do
+      before(:each) { subject.provider = 'google_oauth2' }
+      let(:info_params) { {} }
+      let(:credentials_params) { {} }
+      let(:auth_hash) { OmniAuth::AuthHash.new({provider: 'google_oauth2', info: info_params, credentials: credentials_params}) }
+
+      it 'should set the oauth_token and oauth_expires values' do
+        expires = 3.months.since
+        credentials_params[:token] = 'example_token'
+        credentials_params[:expires_at] = expires.to_i
+        subject.update_from_auth_hash(auth_hash)
+
+        subject.oauth_token.should eq('example_token')
+        subject.oauth_expires_at.to_s.should eq(expires.to_s)
+      end
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,86 +1,155 @@
 require 'spec_helper'
 
 describe User do
+  include OmniauthHelpers
+
+  describe 'factories' do
+    it 'creates a User with basic details' do
+      user = FactoryGirl.create(:user)
+      user.name.should_not be_blank
+      user.email.should_not be_blank
+      user.encrypted_password.should_not be_nil
+    end
+  end
+
+  describe 'Associations' do
+    it { should have_many(:accounts).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_acceptance_of(:terms_and_conditions) }
+  end
 
   describe '.new_with_session' do
     context 'with developer auth session data' do
-      let(:developer_data) do
-        OmniAuth::AuthHash.new({
-          :provider => 'developer',
-          :uid => 'developer@example.com',
-          :info => {
-            :name => 'Test Developer',
-            :email => 'developer@example.com'
-          }
-        })
-      end
-      let(:session) do
-        {
-          'devise.developer_data' => developer_data
-        }
-      end
-      it 'builds a new user with pre-populated details' do
+      let(:session) {{ 'devise.developer_data' => developer_auth_hash }}
+      it 'builds a new user using values from the auth hash' do
         user = User.new_with_session({}, session)
-        user.name.should eq('Test Developer')
-        user.email.should eq('developer@example.com')
+        user.name.should eq(auth_account[:name])
+        user.email.should eq(auth_account[:email])
       end
-      it 'it creates a new account on save' do
+      it 'creates a new developer account on save' do
         user_params = FactoryGirl.attributes_for(:user)
         user = User.new_with_session(user_params, session)
         user.save!
-
-        user.accounts.count.should be(1)
+        user.has_provider_account?('developer').should be_true
+      end
+    end
+    context 'with Facebook auth session data' do
+      let(:session) {{ 'devise.facebook_data' => facebook_auth_hash }}
+      it 'builds a new user using values from the auth hash' do
+        user = User.new_with_session({}, session)
+        user.name.should eq(facebook_credentials[:name])
+        user.email.should eq(auth_account[:email])
+      end
+      it 'creates a new developer account on save' do
+        user_params = FactoryGirl.attributes_for(:user)
+        user = User.new_with_session(user_params, session)
+        user.save!
+        user.has_provider_account?('facebook').should be_true
+      end
+    end
+    context 'with Twitter auth session data' do
+      let(:session) {{ 'devise.twitter_data' => twitter_auth_hash }}
+      it 'builds a new user using values from the auth hash' do
+        user = User.new_with_session({}, session)
+        user.name.should eq(twitter_credentials[:name])
+        user.email.should be_nil # Twitter doesn't include email
+      end
+      it 'creates a new developer account on save' do
+        user_params = FactoryGirl.attributes_for(:user)
+        user = User.new_with_session(user_params, session)
+        user.save!
+        user.has_provider_account?('twitter').should be_true
+      end
+    end
+    context 'with GitHub auth session data' do
+      let(:session) {{ 'devise.github_data' => github_auth_hash }}
+      it 'builds a new user using values from the auth hash' do
+        user = User.new_with_session({}, session)
+        user.name.should eq(github_credentials[:name])
+        user.email.should eq(auth_account[:email])
+      end
+      it 'creates a new developer account on save' do
+        user_params = FactoryGirl.attributes_for(:user)
+        user = User.new_with_session(user_params, session)
+        user.save!
+        user.has_provider_account?('github').should be_true
+      end
+    end
+    context 'with Google auth session data' do
+      let(:session) {{ 'devise.google_oauth2_data' => google_auth_hash }}
+      it 'builds a new user using values from the auth hash' do
+        user = User.new_with_session({}, session)
+        user.name.should eq(google_credentials[:name])
+        user.email.should eq(auth_account[:email])
+      end
+      it 'creates a new developer account on save' do
+        user_params = FactoryGirl.attributes_for(:user)
+        user = User.new_with_session(user_params, session)
+        user.save!
+        user.has_provider_account?('google_oauth2').should be_true
       end
     end
   end
 
-  it { should validate_presence_of(:name) }
-
-  describe '#facebook_login?' do
+  describe '#has_provider_account?' do
     subject { FactoryGirl.create(:user) }
     it 'returns false with no accounts' do
-      subject.facebook_login?.should be_false 
+      subject.has_provider_account?('facebook').should be_false 
+      subject.has_provider_account?('twitter').should be_false 
+      subject.has_provider_account?('github').should be_false 
+      subject.has_provider_account?('google_oauth2').should be_false 
     end
-    it 'returns true with a facebook account' do
-      FactoryGirl.create(:facebook_account, user: subject)
-      subject.facebook_login?.should be_true
+    context 'with a Facebook account' do
+      before(:each) { FactoryGirl.create(:facebook_account, user: subject) }
+      it 'returns true for facebook' do
+        subject.has_provider_account?('facebook').should be_true
+      end
+    end
+    context 'with a Twitter account' do
+      before(:each) { FactoryGirl.create(:twitter_account, user: subject) }
+      it 'returns true for twitter' do
+        subject.has_provider_account?('twitter').should be_true
+      end
+    end
+    context 'with a GitHub account' do
+      before(:each) { FactoryGirl.create(:github_account, user: subject) }
+      it 'returns true for github' do
+        subject.has_provider_account?('github').should be_true
+      end
+    end
+    context 'with a Google account' do
+      before(:each) { FactoryGirl.create(:google_account, user: subject) }
+      it 'returns true for google_oauth2' do
+        subject.has_provider_account?('google_oauth2').should be_true
+      end
     end
   end
 
-  describe '#github_login?' do
-    subject { FactoryGirl.create(:user) }
-    it 'returns false with no accounts' do
-      subject.github_login?.should be_false 
-    end
-    it 'returns true with a google account' do
-      FactoryGirl.create(:github_account, user: subject)
-      subject.github_login?.should be_true
+  describe '#primary_account' do
+    it 'returns the first account' do
+      first_account = FactoryGirl.build_stubbed(:account)
+      subject.stub(:accounts).and_return([first_account, FactoryGirl.build_stubbed(:account)])
+      subject.primary_account.should be(first_account)
     end
   end
 
-  describe '#google_login?' do
-    subject { FactoryGirl.create(:user) }
-    it 'returns false with no accounts' do
-      subject.google_login?.should be_false 
+  describe '#profile_picture' do
+    it 'returns the primary account image' do
+      account_image = 'http://graph.facebook.com/1234567/picture?type=square'
+      account = FactoryGirl.build_stubbed(:account)
+      account.stub(:profile_picture).and_return(account_image)
+
+      subject.stub(:primary_account).and_return(account)
+      subject.profile_picture.should be(account_image)
     end
-    it 'returns true with a google account' do
-      FactoryGirl.create(:google_account, user: subject)
-      subject.google_login?.should be_true
+    it 'reverts back to a gravatar image unique to email' do
+      subject.email = 'bemo56@hotmail.com'
+       # Gravatar hash of bemo56@hotmail.com
+      subject.profile_picture.should eq('http://gravatar.com/avatar/63095bd9974641871e51b92ef72b20a8.png?s=128')
     end
   end
-
-  describe '#twitter_login?' do
-    subject { FactoryGirl.create(:user) }
-    it 'returns false with no accounts' do
-      subject.twitter_login?.should be_false 
-    end
-    it 'returns true with a twitter account' do
-      FactoryGirl.create(:twitter_account, user: subject)
-      subject.twitter_login?.should be_true
-    end
-  end
-
-  # terms_and_conditions
-  it { should validate_acceptance_of(:terms_and_conditions) }
 
 end
