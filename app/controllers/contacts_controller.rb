ecc8234911a9ecb39707994d66f91236f0bcf068
Ben Morrall <bemo56@hotmail.com>
2014-03-02 16:36:21 +1100

    Removed UriHelper as it was causing issues

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,4 @@
 class ContactsController < ApplicationController
-  include ActionView::Helpers::UrlHelper
 
   def show
     @contact = Contact.new
@@ -22,6 +21,32 @@ class ContactsController < ApplicationController
       redirect_to thank_you_contact_path
     else
       render :show
+    end
+  end
+
+  protected
+
+  def current_page?(options)
+    unless request
+      raise "You cannot use helpers that need to determine the current " \
+            "page unless your view context provides a Request object " \
+            "in a #request method"
+    end
+
+    return false unless request.get? || request.head?
+
+    url_string = URI.parser.unescape(url_for(options)).force_encoding(Encoding::BINARY)
+
+    # We ignore any extra parameters in the request_uri if the
+    # submitted url doesn't have any either. This lets the function
+    # work with things like ?order=asc
+    request_uri = url_string.index("?") ? request.fullpath : request.path
+    request_uri = URI.parser.unescape(request_uri).force_encoding(Encoding::BINARY)
+
+    if url_string =~ /^\w+:\/\//
+      url_string == "#{request.protocol}#{request.host_with_port}#{request_uri}"
+    else
+      url_string == request_uri
     end
   end
 
