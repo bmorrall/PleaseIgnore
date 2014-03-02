40b35a5d1fd59c4b55f95e66aedf07c6410c99ad
Ben Morrall <bemo56@hotmail.com>
2014-03-02 15:32:13 +1100

    Added Request spec for Contact form

---
+++ spec/requests/contact_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "Contact" do
+  enable_rails_cache
+
+  describe "GET /contact" do
+    context "as a visitor" do
+      it "renders the contact page" do
+        get contact_path
+        response.status.should be(200)
+      end
+    end
+    describe 'Metadata' do
+      it 'includes the body class' do
+        get contact_path
+        assert_select 'body.contacts.contacts-show'
+      end
+      it 'includes the page title' do
+        get contact_path
+        assert_select 'title', 'PleaseIgnore | Contact us'
+      end
+    end
+  end
+
+end
