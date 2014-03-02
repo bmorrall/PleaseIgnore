67ee05b61874f9dced6426a1fc0a01e050209efb
Ben Morrall <bemo56@hotmail.com>
2014-03-02 22:47:04 +1100

    Visiter has factory girl attributes

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -6,12 +6,19 @@ def destroy_session
 end
 
 def create_visitor
-  @visitor ||= { :name => "Testy McUserton", :email => "example@example.com",
-    :password => "changeme", :password_confirmation => "changeme" }
+  @visitor ||= begin
+    visitor_attributes = FactoryGirl.attributes_for(:user)
+    {
+      :name => visitor_attributes[:name],
+      :email => "example@example.com",
+      :password => "changeme",
+      :password_confirmation => "changeme"
+    }
+  end
 end
 
 def find_user
-  @user ||= User.where(:email => @visitor[:email]).first
+  @user ||= User.where(:email => create_visitor[:email]).first
 end
 
 def create_unconfirmed_user
@@ -22,18 +29,18 @@ def create_unconfirmed_user
 end
 
 def create_user
-  create_visitor
   delete_user
-  @user = FactoryGirl.create(:user, @visitor)
+  @user = FactoryGirl.create(:user, create_visitor)
 end
 
 def delete_user
-  @user ||= User.where(:email => @visitor[:email]).first
+  @user ||= find_user
   @user.destroy unless @user.nil?
+  @visitor = nil
 end
 
 def sign_up
-  delete_user
+  create_visitor
   visit '/users/sign_up'
   fill_in "user_name", :with => @visitor[:name]
   fill_in "user_email", :with => @visitor[:email]
@@ -45,6 +52,7 @@ def sign_up
 end
 
 def sign_in
+  create_visitor
   visit '/users/sign_in'
   fill_in "user_email", :with => @visitor[:email]
   fill_in "user_password", :with => @visitor[:password]
@@ -52,6 +60,7 @@ def sign_in
 end
 
 def reset_password
+  create_visitor
   visit '/users/password/new'
   fill_in "user_email", :with => @visitor[:email]
   click_button 'Reset Password'
