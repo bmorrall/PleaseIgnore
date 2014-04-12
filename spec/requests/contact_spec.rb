7d2cc3a5ae8e5d5ca5907a15f504a21825839112
Ben Morrall <bemo56@hotmail.com>
2014-04-12 15:41:25 +1000

    Updated contact requests

--- spec/requests/contact_spec.rb
+++ spec/requests/contact_spec.rb
@@ -3,11 +3,19 @@ require 'spec_helper'
 describe "Contact" do
   enable_rails_cache
 
+  def valid_create_attributes
+    FactoryGirl.attributes_for(:contact)
+  end
+
   describe "GET /contact" do
     context "as a visitor" do
       it "renders the contact page" do
         get contact_path
         expect(response.status).to be(200)
+      end
+      it 'notifies the user their previously visited url will be included' do
+        get contact_path, nil, { 'HTTP_REFERER' => 'http://example.com/privacy' }
+        assert_select '.alert.alert-info strong', 'Your message will mention you visited this page from http://example.com/privacy'
       end
     end
     describe 'Metadata' do
@@ -18,6 +26,21 @@ describe "Contact" do
       it 'includes the page title' do
         get contact_path
         assert_select 'title', 'PleaseIgnore | Contact us'
+      end
+    end
+  end
+
+  describe 'POST /contact' do
+    context 'as a visitor' do
+      it 'redirects the user to a thank you page' do
+        post contact_path(:contact => valid_create_attributes)
+        expect(response).to redirect_to thank_you_contact_path
+      end
+      it 'notifies the user that their contact request was sent' do
+        post contact_path(:contact => valid_create_attributes)
+        follow_redirect!
+
+        assert_select '.alert.alert-success strong', 'Your contact request has been sent'
       end
     end
   end
