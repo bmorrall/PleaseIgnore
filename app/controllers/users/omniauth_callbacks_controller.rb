e6fc24649502a280e5f67dbed8c8f4edb5011ff8
Ben Morrall <bemo56@hotmail.com>
2014-04-13 13:57:25 +1000

    Cleaned up omniauth callbacks controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -30,10 +30,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     account.user = current_user
     if account.save
       # Account has been linked to profile
-      set_flash_message(:notice, :success, :kind => Account::provider_name(provider)) if is_navigational_format?
+      display_authenticated_flash_message provider
     else
       # Account has been linked to another profile
-      set_flash_message(:alert, :failure, :kind => Account::provider_name(provider), :reason => 'Someone has already linked to this account') if is_navigational_format?
+      display_failure_flash_message 'Someone has already linked to this account', provider
     end
     redirect_to edit_user_registration_path
   end
@@ -41,19 +41,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def sign_in_or_register_account(auth_hash, provider)
     account = Account.find_for_oauth(auth_hash, provider)
     if account.present?
-      if account.enabled?
-        # Sign in to profile owning linked account
-        set_flash_message(:notice, :success, :kind => Account::provider_name(provider)) if is_navigational_format?
-        sign_in_and_redirect account.user, :event => :authentication
-      else
-        set_flash_message(:alert, :failure, :kind => Account::provider_name(provider), :reason => 'This account has been disabled') if is_navigational_format?
-        redirect_to new_user_session_path
-      end
+      sign_user_into_account account, provider
     else
       # Redirect to User Registration with auth hash
-      set_flash_message(:notice, :success, :kind => Account::provider_name(provider)) if is_navigational_format?
-      session["devise.#{provider}_data"] = auth_hash.reject{ |k| k.to_sym == :raw_info }
-      redirect_to new_user_registration_path
+      redirect_user_to_registration_page auth_hash, provider
+    end
+  end
+
+  def sign_user_into_account(account, provider)
+    if account.enabled?
+      # Sign in to profile owning linked account
+      display_authenticated_flash_message provider
+      sign_in_and_redirect account.user, :event => :authentication
+    else
+      display_failure_flash_message 'This account has been disabled', provider
+      redirect_to new_user_session_path
     end
   end
 
@@ -61,14 +63,40 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     account = Account.find_for_oauth(auth_hash, provider)
     if account.present?
       # Deny Authentication from this Provider
-      set_flash_message(:alert, :failure, :kind => Account::provider_name(provider), :reason => 'Authentication is disabled from this Provider') if is_navigational_format?
+      display_failure_flash_message 'Authentication is disabled from this Provider', provider
       redirect_to new_user_session_path
     else
       # Redirect to User Registration with auth hash
-      set_flash_message(:notice, :success, :kind => Account::provider_name(provider)) if is_navigational_format?
-      session["devise.#{provider}_data"] = auth_hash.reject{ |k| k.to_sym == :raw_info }
-      redirect_to new_user_registration_path
+      redirect_user_to_registration_page auth_hash, provider
     end
+  end
+
+  def redirect_user_to_registration_page(auth_hash, provider)
+    display_registration_flash_message provider
+    save_auth_hash_to_session auth_hash, provider
+    redirect_to new_user_registration_path
+  end
+
+  private
+  
+  def save_auth_hash_to_session(auth_hash, provider)
+    # Save the auth hash without raw info
+    session["devise.#{provider}_data"] = auth_hash.reject{ |key| key.to_sym == :raw_info }
+  end
+
+  # Successful authentication, but registration is required
+  def display_registration_flash_message(provider)
+    # TODO: Registration message
+    display_authenticated_flash_message provider
+  end
+
+  # Successful authentication with the user being logged in
+  def display_authenticated_flash_message(provider)
+    set_flash_message(:notice, :success, :kind => Account::provider_name(provider)) if is_navigational_format?
+  end
+
+  def display_failure_flash_message(reason, provider)
+    set_flash_message(:alert, :failure, :kind => Account::provider_name(provider), :reason => reason) if is_navigational_format?
   end
 
 end
