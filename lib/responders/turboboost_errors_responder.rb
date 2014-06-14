cc689148e927fdd20fc8a13b55cccf1c25f4f2fa
Ben Morrall <bemo56@hotmail.com>
2014-06-14 23:01:27 +1000

    Removed unused responer method

--- lib/responders/turboboost_errors_responder.rb
+++ lib/responders/turboboost_errors_responder.rb
@@ -8,13 +8,5 @@ module Responders
         defined?(super) ? super : to_format
       end
     end
-
-    def to_js
-      if !get? && has_errors?
-        controller.render_turboboost_errors_for(resource)
-      else
-        defined?(super) ? super : to_format
-      end
-    end
   end
 end
