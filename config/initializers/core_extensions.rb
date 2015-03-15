41560e7b5e85c566781095973c121e1fb10361c4
Ben Morrall <bemo56@hotmail.com>
2015-03-15 21:32:27 +1100

    Added String#safe_permalink convenience method

--- config/initializers/core_extensions.rb
+++ config/initializers/core_extensions.rb
@@ -1,4 +1,6 @@
 # Core Extensions added to the app
+require 'core_extensions/string/safe_permalink'
 require 'core_extensions/string/transliterations'
 
+String.include CoreExtensions::String::SafePermalink
 String.include CoreExtensions::String::Transliterations
---
+++ lib/core_extensions/string/safe_permalink.rb
@@ -0,0 +1,20 @@
+module CoreExtensions
+  module String
+    # Adds Convenience method #safe_permalink to string, which:
+    # - downcases, strips non-ascii characters
+    # - converts invalid characters to single '-' strings
+    module SafePermalink
+      # Converts string into a safe permalink, relies on #transliterate
+      def safe_permalink
+        # permalink has characters normalised from non-latin characters
+        transliterate.tap do |permalink|
+          permalink.scrub!('-') # Remove remaining non utf-8 characters
+          permalink.downcase! # Downcase all values
+          permalink.gsub!(/[^a-z0-9]+/, '-') # Replace duplicate invalid characters with single '-'
+          permalink.sub!(/\A-/, '') # Remove any leading '-' characters
+          permalink.chomp!('-') # Remove any trailing '-' characters
+        end
+      end
+    end
+  end
+end
