c046097f3d9d50a60fc9a1824bce31c98193abca
Ben Morrall <bemo56@hotmail.com>
2015-05-03 15:40:34 +1000

    Removed session from API requests

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -5,16 +5,21 @@ module Api
       # Prevent CSRF attacks by clearning the session..
       protect_from_forgery with: :null_session
 
-      concerning :Versions do
-        protected
+      before_action :destroy_session
 
-        # [PaperTrail] include IP and User Agent in version history
-        def info_for_paper_trail
-          {
-            ip: request.remote_ip,
-            user_agent: request.user_agent
-          }
-        end
+      protected
+
+      # Prevents the API from setting anything to the session
+      def destroy_session
+        request.session_options[:skip] = true
+      end
+
+      # [PaperTrail] include IP and User Agent in version history
+      def info_for_paper_trail
+        {
+          ip: request.remote_ip,
+          user_agent: request.user_agent
+        }
       end
     end
   end
