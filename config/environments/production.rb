befc29e2cd52eee481ded8ea7d6b6a7c644cf8fb
Ben Morrall <bemo56@hotmail.com>
2015-06-26 09:54:03 +1000

    Raised debug level to info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true if ENV['SSL_ENABLED']
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = :debug
+  # Rely on third parties for detailed debugging information
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
