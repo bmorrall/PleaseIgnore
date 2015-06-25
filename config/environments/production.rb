e85b5f08c69963717b4f9bb0e70102e02e76c102
Ben Morrall <bemo56@hotmail.com>
2015-06-25 21:01:03 +1000

    Docker container now uses Send-File

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,9 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  unless ENV['RAILS_SERVE_STATIC_FILES'].present?
+    config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  end
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true if ENV['SSL_ENABLED']
