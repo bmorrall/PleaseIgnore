19337823827b8ccc919f5bc957b96cde684793fd
Ben Morrall <bemo56@hotmail.com>
2015-06-27 21:34:20 +1000

    Removed Dashboard link from sidenav

--- spec/features/users/version_history_spec.rb
+++ spec/features/users/version_history_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'User Version History', type: :feature do
   scenario 'Review Account History', :login_admin do
     # When I view my account history
-    click_link 'History'
+    visit_account_history_page
 
     # Then I should see a user created Version
     assert_selector '.versions-list .list-group-item-heading',
@@ -16,7 +16,7 @@ feature 'User Version History', type: :feature do
 
   scenario 'View Version Summary', :js, :login_admin do
     # When I view my account history
-    click_link 'History'
+    visit_account_history_page
 
     # And I click on the User Create Version
     find(
@@ -30,5 +30,18 @@ feature 'User Version History', type: :feature do
       expect(page).to have_content logged_in_user.email
       expect(page).to have_content logged_in_user.name
     end
+  end
+
+  def visit_account_history_page
+    within 'nav.navbar' do
+      if page.has_selector?('nav.navbar a', text: t('layouts.navigation.my_dashboard'))
+        # Escape from static pages
+        click_link t('layouts.navigation.my_dashboard')
+      end
+      click_link logged_in_user.name
+      click_link t('layouts.navigation.my_profile')
+    end
+
+    click_link 'History'
   end
 end
