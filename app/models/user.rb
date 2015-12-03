f0e95a0f1a875d3d08455b255ccfc6bf2c0ee17d
Ben Morrall <bemo56@hotmail.com>
2015-12-03 21:57:44 +1100

    Prevented users from authenticating with soft deleted accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -157,6 +157,16 @@ class User < ActiveRecord::Base
       super # Fallback to standard logic
     end
 
+    # ensure user account is active
+    def active_for_authentication?
+      super && !deleted_at?
+    end
+
+    # provide a custom message for a deleted account
+    def inactive_message
+      !deleted_at? ? super : :deleted_account
+    end
+
     protected
 
     def allow_empty_password?
