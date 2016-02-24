51dacce7e91b23b5e1f85b4499bc0acbe6c03261
Ben Morrall <bemo56@hotmail.com>
2016-02-24 12:26:02 +1100

    Added metadata to exceptions

--- engines/logging/lib/logging/exception_notifier.rb
+++ engines/logging/lib/logging/exception_notifier.rb
@@ -7,20 +7,20 @@ module Logging
       STDERR.puts "#{name}: Unable to load 'rollbar' please include it in your Gemfile"
     end
 
-    def self.notify(error, _additional_fields = {})
-      notify_rollbar(error)
-      notify_logger(error)
+    def self.notify(error, additional_fields = {})
+      notify_rollbar(error, additional_fields)
+      notify_logger(error, additional_fields)
     end
 
-    def self.notify_logger(exception)
+    def self.notify_logger(exception, additional_fields)
       message = exception.message
       backtrace = exception.backtrace.try(:join, "\n")
-      Logging.error "#{exception.class.name}\n#{message}\n#{backtrace}"
+      Logging.error "#{exception.class.name}\n#{message}\n#{backtrace}", additional_fields
     end
 
-    def self.notify_rollbar(error)
+    def self.notify_rollbar(error, additional_fields)
       return false unless defined?(Rollbar)
-      Rollbar.error(error, use_exception_level_filters: true)
+      Rollbar.error(error, additional_fields.reverse_merge(use_exception_level_filters: true))
     end
   end
 end
